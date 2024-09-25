0b7328e26158389f2cc1015f53b32b0f5476c474
kinasih20 <agnesalfa04@gmail.com>
2024-09-25 11:42:50 +0700

    viewdata

--- resources/views/admin/viewdata.blade.php
+++ resources/views/admin/viewdata.blade.php
@@ -14,50 +14,52 @@
             <a href="{{ route('admin.dashboard') }}" class="btn btn-secondary">Back</a>
         </div>
         @foreach($transactions as $transaction)
-        <div class="card">
-            <div class="card-header bg-info text-white">
-                <h3 class="card-title">Detail Transaksi Laundry</h3>
+            <div class="card">
+                <div class="card-header bg-info text-white">
+                    <h3 class="card-title">Detail Transaksi Laundry</h3>
+                </div>
+                <div class="card-body">
+                    <div class="row mb-3">
+                        <div class="col-sm-4 font-weight-bold">Tanggal Masuk</div>
+                        <div class="col-sm-8">{{ $transaction->tanggal_masuk }}</div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-sm-4 font-weight-bold">Nama Costumer</div>
+                        <div class="col-sm-8">{{ $transaction->nama_pelanggan }}</div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-sm-4 font-weight-bold">Jenis Layanan</div>
+                        <div class="col-sm-8">{{ $transaction->jenis_layanan }}</div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-sm-4 font-weight-bold">Jenis Laundry</div>
+                        <div class="col-sm-8">{{ $transaction->jenis_laundry }}</div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-sm-4 font-weight-bold">Berat (kg)</div>
+                        <div class="col-sm-8">{{ $transaction->berat }}</div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-sm-4 font-weight-bold">Metode Pembayaran</div>
+                        <div class="col-sm-8">{{ $transaction->metode_pembayaran }}</div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-sm-4 font-weight-bold">Total Harga</div>
+                        <div class="col-sm-8">Rp {{ $transaction->total_harga }}</div>
+                    </div>
+                    <div class="text-right">
+                        <a href="{{ route('admin.editdata', ['id' => $transaction->id]) }}" class="btn btn-warning">Edit
+                            Transaksi</a>
+                        <form action="{{ route('admin.delete', ['id' => $transaction->id]) }}" method="POST"
+                            style="display: inline;">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Hapus Transaksi</button>
+                        </form>
+                    </div>
+                </div>
             </div>
-            <div class="card-body">
-                <div class="row mb-3">
-                    <div class="col-sm-4 font-weight-bold">Tanggal Masuk</div>
-                    <div class="col-sm-8">{{ $transaction->tanggal_masuk }}</div>
-                </div>
-                <div class="row mb-3">
-                    <div class="col-sm-4 font-weight-bold">Nama Pelanggan</div>
-                    <div class="col-sm-8">{{ $transaction->nama_pelanggan }}</div>
-                </div>
-                <div class="row mb-3">
-                    <div class="col-sm-4 font-weight-bold">Jenis Layanan</div>
-                    <div class="col-sm-8">{{ $transaction->jenis_layanan }}</div>
-                </div>
-                <div class="row mb-3">
-                    <div class="col-sm-4 font-weight-bold">Jenis Laundry</div>
-                    <div class="col-sm-8">{{ $transaction->jenis_laundry }}</div>
-                </div>
-                <div class="row mb-3">
-                    <div class="col-sm-4 font-weight-bold">Berat (kg)</div>
-                    <div class="col-sm-8">{{ $transaction->berat }}</div>
-                </div>
-                <div class="row mb-3">
-                    <div class="col-sm-4 font-weight-bold">Metode Pembayaran</div>
-                    <div class="col-sm-8">{{ $transaction->metode_pembayaran }}</div>
-                </div>
-                <div class="row mb-3">
-                    <div class="col-sm-4 font-weight-bold">Total Harga</div>
-                    <div class="col-sm-8">Rp {{ $transaction->total_harga }}</div>
-                </div>
-                <div class="text-right">
-                    <a href="{{ route('admin.editdata', ['id' => $transaction->id]) }}" class="btn btn-warning">Edit Transaksi</a>
-                    <form action="{{ route('admin.delete', ['id' => $transaction->id]) }}" method="POST" style="display: inline;">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger">Hapus Transaksi</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-        <br>
+            <br>
         @endforeach
     </div>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
